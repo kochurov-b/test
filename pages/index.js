ab65065581adf49b054aa6b7ea9364a33d5cb46b
kochurov-b <kochurov.b@gmail.com>
2021-09-17 16:37:05 +0400

    alert

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
@@ -19,6 +20,19 @@ export default function Index() {
         <ProTip />
         <Copyright />
       </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          width: '100%',
+        }}
+        onClick={() => alert('Click')}
+      >
+        Click
+      </Button>
     </Container>
   );
 }
