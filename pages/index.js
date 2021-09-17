fa2732387d2eeff73693fb52274675cce165493e
kochurov-b <kochurov.b@gmail.com>
2021-09-17 17:55:54 +0400

    new meta

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Index() {
       <NextHead>
         <meta
           name="viewport"
-          content="width=device-width, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1, user-scalable=no"
         />
       </NextHead>
       <Container maxWidth="sm">
